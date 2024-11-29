74e796ae5713aa37578b89fecf5fa5a4415f03f6
julio <juliofariadev@gmail.com>
2024-11-29 15:59:14 -0300

    private and public properties

--- Classes-and-interfaces/app.ts
+++ Classes-and-interfaces/app.ts
@@ -32,3 +32,33 @@
 // accountingCopy.describe()
 // console.log(accountingCopy.describe)
 
+// PRIVATE AND PUBLIC ACCESS MODIFIERS
+
+class Department {
+    name: string;
+    private employees: string[] = [];
+
+    constructor(n: string) {
+        this.name = n;
+    }
+
+    describe(this: Department) {
+        console.log('Department: ' + this.name)
+    }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length)
+        console.log(this.employees)
+    }
+}
+
+const accounting = new Department('Accounting');
+accounting.addEmployee('Max')
+accounting.addEmployee('Manu')
+
+accounting.describe();
+accounting.printEmployeeInformation();
