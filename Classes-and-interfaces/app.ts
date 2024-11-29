79249a1f21cd0224e7ec85adbaf486073bfd3913
julio <juliofariadev@gmail.com>
2024-11-29 15:31:10 -0300

    Practice create classes

---
+++ Classes-and-interfaces/app.ts
@@ -0,0 +1,15 @@
+// class UserCredentials {
+//     name: string;
+//     password: string;
+//
+//     constructor(name: string, password: string) {
+//         this.name = name;
+//         this.password = password
+//     }
+// }
+//
+// const julio = new UserCredentials('julioteste@gmail.com', 'teste123');
+// const bia = new UserCredentials('bia@gmail.com', 'teste123')
+// console.log(julio)
+// console.log(bia)
+
